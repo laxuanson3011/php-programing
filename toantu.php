8126550b7f42b5f40e16d9e50e4785da0a011ae3
Xuan Son <xuanson.tth.dn@gmail.com>
2017-10-12 16:44:56 +0700

    toan tu

---
+++ toantu.php
@@ -0,0 +1,25 @@
+<?php
+	$a = 10;
+	$b = 5;
+	$tong = $a + $b;
+	echo "$a + $b = $tong";
+
+	$hieu = $a - $b;
+	echo "$a - $b = $hieu";
+
+	$tich = $a * $b;
+	echo "$a * $b = $tich";
+
+	$thuong = $a / $b;
+	echo "$a / $b = $tich";
+
+	$du = $a % $b;
+	echo "$a % $b = $du";
+
+	echo "Tang va giam";
+	$giam = --$a;
+	echo "--$a = $giam";
+
+	$tang = ++$a;
+	echo "++$a = $tang";
+?>
