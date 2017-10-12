8ea54ca63c281cd9c0f5d8b0516b8d2f8b1aa3bf
Xuan Son <xuanson.tth.dn@gmail.com>
2017-10-12 16:13:26 +0700

    bien trong php

---
+++ cut.php
@@ -0,0 +1,13 @@
+<?php
+	define("PI", 3.14);
+	define("SON","lxSON");
+	$name = "LA XUAN SON";
+	$Tuoi = 25;
+	$Diachi = "HUE";
+	echo "TEN: $name <br>";
+	echo "----------------<br>";
+	echo "TUOI: $Tuoi <br>";
+	echo "----------------<br>";
+	echo "DIACHI: $Diachi <br>";
+	ECHO SON;
+?>
